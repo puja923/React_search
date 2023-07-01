13f7e647f4280e329947cd554435b05427c9401a
Puja Ahuja <ahujapuja55@gmail.com>
2023-07-01 18:33:37 +0530

    search-engine

---
+++ src/Components/SearchResultsList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchResult from './SearchResult'
+import "./SearchResultsList.css"
+
+const SearchResultsList = ({results}) => {
+  return (
+    <div className="results-list">
+      {results.map((result, id)=>{
+        return <SearchResult result={result.name} key={id}/>
+      })}
+    </div>
+  )
+}
+
+export default SearchResultsList
